13ec6a28ca59f2d8ee9c6337c80ed428df027a7a
hugues0 <ntwari.hugues@gmail.com>
2021-10-16 19:23:42 +0200

    add logger and morgan system logs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import { config } from 'dotenv'
-import express from 'express'
-import cors from 'cors'
-import employeesRoute from './routes/employeesRoute.js'
-import managersRoute from './routes/managersRoute.js'
+import { config } from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import logger from './helpers/logger';
+import morgan from 'morgan';
+import employeesRoute from './routes/employeesRoute.js';
+import managersRoute from './routes/managersRoute.js';
 
 config()
 
 const app = express()
 app.use(cors())
+app.use(morgan("tiny", { stream: logger.stream }));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/employees",employeesRoute)
